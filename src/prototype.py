f201ab4a2cb01147c7dfa54e9afc75c47c2d4770
Théo Matricon <theomatricon@gmail.com>
2025-09-03 11:57:35 +0200

    added basic logging

--- src/prototype.py
+++ src/prototype.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
+import logging
 
 from sustainablecompetition.benchmarkatoms import Job, JobState, JobStateError, Result
 from sustainablecompetition.benchmarkinterfaces import Benchmarker, Infrastructure
+
+logger = logging.getLogger(__name__)
 
 
 def main():
--- src/sustainablecompetition/solveradapters/satsolver.py
+++ src/sustainablecompetition/solveradapters/satsolver.py
@@ -4,12 +4,13 @@ SAT Solver Adapter
 
 from sustainablecompetition.solveradapters.abstractsolver import AbstractSolverAdapter
 
+
 class SATSolverAdapter(AbstractSolverAdapter):
-    """ Maintain paths to solvers and make them accessible by their IDs """
+    """Maintain paths to solvers and make them accessible by their IDs"""
 
     # Maps solver ids to solver paths
     registry = {}
-    
+
     def get_path(self, solver_id: str) -> str:
         """
         Get the file path for a given solver ID.
