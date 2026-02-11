044b8b31afa80acbb77a5370040b11eab1b122c9
MarieNyst <marie.anastacio@gmail.com>
2026-02-11 10:23:24 +0100

    cleaning up old scripts

--- workdir/run_solver.sh
+++ workdir/run_solver.sh
@@ -5,12 +5,12 @@
 
 # Check if the correct number of arguments is provided
 if [ "$#" -ne 5 ]; then
-    echo "Usage: $0 <environment_hash> <executable_path> <instance_path> <memory> <cutoff>"
+    echo "Usage: $0 <environment_id> <executable_path> <instance_path> <memory> <cutoff>"
     exit 1
 fi
 
 # Assign arguments
-environment_hash=$1
+environment_id=$1
 executable_path=$2
 instance_path=$3
 memory=$4
@@ -24,7 +24,7 @@ instance_filename=$(basename "$instance_path")
 instancehash=$(echo "$instance_filename" | cut -d'-' -f1)
 
 # Construct the output filename
-output_file="${environment_hash}_${name}_${instancehash}.log"
+output_file="${environment_id}_${name}_${instancehash}.log"
 
 # Run the executable and save the output to the constructed filename
 python wrapper.py --solver "$executable_path" --instance "$instance_path" --memory "$memory" --cutoff "$cutoff"
